593fabe9be403110e5e5039aa472835d85e85fc8
unknown <le_gall@220056slocal>
2025-03-14 13:58:45 +0100

    V1

---
+++ ProjetAtlantik/FormAjouterSecteur.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetAtlantik
+{
+    public partial class FormAjouterSecteur : Form
+    {
+        public FormAjouterSecteur()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAjouterSecteur_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(tbxAjouterSecteur.Text);
+        }
+    }
+}
